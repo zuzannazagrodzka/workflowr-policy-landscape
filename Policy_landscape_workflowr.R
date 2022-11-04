35da55d594e05ac37c08789cbb8566c19dd5654c
andbeck <andrew.beckerman@gmail.com>
2022-11-04 16:53:43 +0000

    learning the ropes

--- Policy_landscape_workflowr.R
+++ Policy_landscape_workflowr.R
@@ -1,43 +1,68 @@
 # Connecting to Git and checking credentials
-# gitcreds::gitcreds_set()
+gitcreds::gitcreds_set()
 
 library("workflowr")
 
-# wflow_git_config(user.name = "zuzannazagrodzka", user.email = "zuzanna.zagrodzka@gmail.com", overwrite=TRUE)
+wflow_git_config(user.name = "andbeck",
+                 user.email = "andrew.beckerman@gmail.com",
+                 overwrite=TRUE)
+
+#wflow_start("myproject")
+
 
 # Starting my first ever workflowr project
-# wflow_start("myproject")
 
-# Two folders below I can't detele:
-# analysis/: This directory contains all the source R Markdown files for implementing the data analyses for your project.
-# docs/: This directory contains all the HTML files for your website.
-# Other
 
-# data/: This directory is for raw data files.
+# Two folders below I can't delete:
 
-# code/: This directory is for code and scripts that might not be appropriate as R Markdown notebooks (e.g. for pre-processing the data, or for long-running code).
+## analysis/: This directory contains all the source R Markdown files for implementing the data analyses for your project.
+## docs/: This directory contains all the HTML files for your website.
 
-# output/: This directory is for processed data files and other outputs generated from the code and data. For example, scripts in code that pre-process raw data files from data/ should save the processed data files in output/.
+## Other
+
+## data/: This directory is for raw data files.
+
+## code/: This directory is for code and scripts that might not be
+# appropriate as R Markdown notebooks (e.g. for pre-processing the data, or for long-running code).
+
+## output/: This directory is for processed data files and other outputs generated
+# from the code and data. For example, scripts in code that pre-process raw data
+# files from data/ should save the processed data files in output/.
 
 # Building the website
-# wflow_build()
-# View the website without buiding it first
+wflow_build(c(
+  "analysis/index.Rmd",
+  "analysis/about.Rmd",
+  "analysis/license.Rmd"))
+
+# View the website without building it first
 wflow_view()
+
 # View the status of the workflowr project
 wflow_status()
 
-#  The first argument to wflow_publish() is a character vector of the R Markdown files to publish. The second is a message that will recorded by the version control system Git when it commits (i.e. saves a snapshot of) these files. The more informative the commit message the better (so that future you knows what you were trying to accomplish).
+#  The first argument to wflow_publish()
+# is a character vector of the R Markdown files to publish.
+# The second is a message that will recorded by the version control system Git when
+# it commits (i.e. saves a snapshot of) these files. The more informative the commit message the
+# better (so that future you knows what you were trying to accomplish).
 
 wflow_publish(c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd"), "Publish the initial files for myproject")
+
 wflow_status()
 
 
 # Deploy the website
 
 
-# Next you need to tell your local Git repository about this new GitHub repository. Run the wflow_remotes() command below in the R console, replacing “myname” with your GitHub username:
+# Next you need to tell your local Git repository about this new GitHub repository.
+# Run the wflow_remotes() command below in the R console, replacing “myname” with your GitHub username:
 # It can be run only once for the project
-wflow_git_remote("origin", user = "zuzannazagrodzka", repo = "workflowr-policy-landscape", action = "set_url")
+
+wflow_git_remote("origin", user = "andbeck",
+                 repo = "workflowr-policy-landscape",
+                 action = "set_url")
+
 # ?wflow_git_remote()
 
 
